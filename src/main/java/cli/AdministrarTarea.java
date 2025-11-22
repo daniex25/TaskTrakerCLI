8b48df3e81c770263389e5c0f4f63fdc161f8e25
Daniel Santamaria <daniexv91@gmail.com>
2025-11-21 22:28:10 -0500

    Método "CargarTareaDeJSON"
    Se agregó el método para recuperar la información del archivo "tareas.json"

--- src/main/java/cli/AdministrarTarea.java
+++ src/main/java/cli/AdministrarTarea.java
@@ -2,18 +2,19 @@ package cli;
 
 import modelo.Tarea;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AdministrarTarea {
     //Se utilizará como memoria temporal antes de guardarlo en el archvio JSON
     private List<Tarea> listaTareas = new ArrayList<Tarea>();
     private static final String NOMBRE_ARCHIVO = "tareas.json";
+
+    public AdministrarTarea(){
+        CargarTareaDeJSON();
+    }
 
     public void AgregarTarea(int id, String descripcion){
         Tarea nuevaTarea = new Tarea(id, descripcion);
@@ -21,7 +22,7 @@ public class AdministrarTarea {
         System.out.println("La tarea fue agregada " + "\n" + nuevaTarea);
     }
 
-    public void GuardarAJSON(){
+    private void GuardarTareaAJSON(){
         //BufferWriter para poder escribir en el archivo tareas.json
         try(BufferedWriter escritor = new BufferedWriter(new FileWriter(NOMBRE_ARCHIVO))) {
             //Abrimos llaves Json
@@ -45,6 +46,44 @@ public class AdministrarTarea {
             System.out.println("Error al guardar el archivo, " + e.getMessage());
         }
     }
+
+    private void CargarTareaDeJSON(){
+        try(BufferedReader lector = new BufferedReader(new FileReader(NOMBRE_ARCHIVO))){
+            //leer la documentación sobre Stream en Java
+            //lines: Métod de BufferedReader que lee todas las líneas del archivo y las devuelve como un Stream<String>.
+            //collect: toma todos los elementos del stream y los reúne en algo
+            //Collectors.joining: Concatena todos los elementos del Stream en una sola cadena sin separadores
+            String json = lector.lines().collect(Collectors.joining());
+            //por lo tanto, ahora el archivo tendra tod0 unido, sin espacios
+
+            if (!json.isEmpty() && !json.equals("{}")){
+                json = json.substring(1, json.length()-1); //devuelve sin los corchetes que pusimos en GuardarTareaAJSON
+                String[] arregloTarea = json.split("},\\{"); //Separamos nuestros objetos en JSON
+                for (String tarea : arregloTarea){
+                    //reemplazamos los {} por vacio
+                    tarea = tarea.replace("{", "").replace("}","");
+                    String[] SplitArreglo = tarea.split(","); //Separamos donde haya una coma
+                    Map<String,String> tareaMap = new HashMap<>();
+                    for (String splitTareas : SplitArreglo){
+                        String[] LlaveValor = splitTareas.split(":");
+                        tareaMap.put(LlaveValor[0].replace("\"", ""),LlaveValor[1].replace("\"",""));
+                    }
+                    Tarea objetoTarea = new Tarea(
+                            Integer.parseInt(tareaMap.get("id")),
+                            tareaMap.get("descripcion")
+                    );
+                    objetoTarea.setEstado(tareaMap.get("estado"));
+                    objetoTarea.setCreadoEn(LocalDateTime.parse(tareaMap.get("creadoEn")));
+                    objetoTarea.setActualizadoEn(LocalDateTime.parse(tareaMap.get("actualizadoEn")));
+
+                    listaTareas.add(objetoTarea);
+                }
+            }
+        }catch (IOException e){
+            System.out.println("Error cargando tareas desde el archivo: " + e.getMessage());
+        }
+    }
+
 
     public void ListarTarea(){
         if (listaTareas.isEmpty()){
